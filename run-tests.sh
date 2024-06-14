fab107d269b1565b985a23b0da7eb7f3893691c3
L-o-rd <115498495+L-o-rd@users.noreply.github.com>
2024-06-14 12:16:14 +0300

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -2,6 +2,8 @@
 
 # Compile main code
 javac -d out -cp "lib/*" src/com/insight/*.java
+javac -d out -cp "lib/*" src/com/insight/graphics/*.java
+javac -d out -cp "lib/*" src/com/insight/states/*.java
 
 # Compile test code
 javac -d out -cp "lib/*:out" src/tests/*.java
